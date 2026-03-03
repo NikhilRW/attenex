984a1eefbcfc634a41ca8fca6ec497a8166c2001
NikhilPersonal-Code <tusharwankhede863@gmail.com>
2026-03-03 12:23:04 +0530

    chore:migrated to azure backend

--- src/shared/constants/uri.ts
+++ src/shared/constants/uri.ts
@@ -4,6 +4,4 @@
 
 export const BASE_URI = __DEV__
   ? "http://localhost:5000"
-  : "https://attenex.up.railway.app";
-
-// export const BASE_URI = "https://attenex.up.railway.app";
+  : process.env.EXPO_PUBLIC_PRODUCTION_BACKEND_URL;
