a1c3cd1b314a30f524437a685cfcc4275c0c30d7
NikhilRW <nikhilwankhede0707@gmail.com>
2026-07-10 00:43:57 +0530

    impr:added basic ssl pinning in the app

--- apps/expo/src/shared/constants/uri.ts
+++ apps/expo/src/shared/constants/uri.ts
@@ -1,7 +1,3 @@
-// For Testing Purposes
-// export const BASE_URI = "http://localhost:5000";
-// export const BASE_URI = "https://attenex-backend.up.railway.app";
-
 export const BASE_URI = __DEV__
   ? "http://localhost:5000"
   : process.env.EXPO_PUBLIC_PRODUCTION_BACKEND_URL;
