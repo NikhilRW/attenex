150d0f31b1a29adc7a858f983445fb4cb2236fa0
NikhilRW <nikhilwankhede0707@gmail.com>
2026-09-15 21:39:21 +0530

    native-sim: 2026-09-15T16:09:21.018Z

--- apps/expo/src/shared/constants/uri.ts
+++ apps/expo/src/shared/constants/uri.ts
@@ -1,5 +1,5 @@
 export const BASE_URI = __DEV__
-  ? "http://localhost:5000"
+  ? "https://sleeve-delivery-unloving.ngrok-free.dev"
   : process.env.EXPO_PUBLIC_PRODUCTION_BACKEND_URL;
 export const ATTENEX_NOTIFICATION_IMAGE_URL =
   "https://attenex.vercel.app/notification-attachment.png";
